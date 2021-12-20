16a2cb23d28231926fda7879fa052ea5247495c3
Masashi Okada <s20c1027tj@s.chibakoudai.jp>
2021-12-20 13:12:19 +0000

    Update

--- myled.c
+++ myled.c
@@ -1,3 +1,6 @@
+//SPDX-Licence-Identifer:GPL3.0
+//*Copyright(c) 2021 Ryuichi Ueda & Masashi Okada.All right reserved.
+
 #include<linux/module.h>
 #include<linux/fs.h>
 #include<linux/cdev.h>
@@ -70,6 +73,7 @@ static struct file_operations led_fops = {
 static int __init init_mod(void)
 {
 	          int retval;
+		  int a = 22;
 		  retval = alloc_chrdev_region(&dev, 0, 1, "myled");
 		  if(retval < 0){
 			  printk(KERN_ERR "alloc_chrdev_region failed.\n");				
@@ -89,32 +93,14 @@ static int __init init_mod(void)
        	         } 
                  device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
 		 gpio_base = ioremap_nocache(0x3f200000, 0xA0);
-		 const u32 led1 = 25;
-		 const u32 index1 = led1/10;
-		 const u32 shift1 = (led1%10)*3;
-		 const u32 mask1 = ~(0x7 << shift1);
-		 gpio_base[index1] = (gpio_base[index1] & mask1) | (0x1 << shift1);
-
-		 const u32 led2 = 24;
-		 const u32 index2 = led2/10;
-		 const u32 shift2 = (led2%10)*3;
-		 const u32 mask2 = ~(0x7 << shift2);
-		 gpio_base[index2] = (gpio_base[index2] & mask2) | (0x1 << shift2);
-
-	 
-		 const u32 led3 = 23;
-		 const u32 index3 = led3/10;		
-		 const u32 shift3 = (led3%10)*3;	
-		 const u32 mask3 = ~(0x7 << shift3);
-		 gpio_base[index3] = (gpio_base[index3] & mask3) | (0x1 << shift3);
-
-		 
-		 const u32 led4 = 22;
-		 const u32 index4 = led4/10;
-		 const u32 shift4 = (led4%10)*3;					
-		 const u32 mask4 = ~(0x7 << shift4);						
-		 gpio_base[index4] = (gpio_base[index4] & mask4) | (0x1 << shift4);
-
+		 while(a < 26){
+			 const u32 led = a;
+			 const u32 index = led/10;
+			 const u32 shift = (led%10)*3;
+			 const u32 mask = ~(0x7 << shift);
+			 gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+			 a++;
+		 }
 		 return 0;
 }
 
